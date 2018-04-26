1027f1adb8a75a250e48398e909175b645143422
Kais Jallouli <kjalloul@42.student.fr>
2018-04-26 08:51:05 +0200

    separated resolve primitive to allow finding distance independantly

--- src/ft_calculate_normal_primitives.c
+++ src/ft_calculate_normal_primitives.c
@@ -6,26 +6,26 @@
 /*   By: kjalloul <kjalloul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/21 14:42:06 by kjalloul          #+#    #+#             */
-/*   Updated: 2018/03/22 12:35:00 by kjalloul         ###   ########.fr       */
+/*   Updated: 2018/04/23 16:54:17 by kjalloul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-void	ft_cylinder_normal(t_prim *prim)
+void	ft_cylinder_normal(t_prim *prim, t_3dpt *p)
 {
 	double	dist;
 	t_3dpt	local_p;
 	t_3dpt	global_o;
 
-	dist = SQR(ft_calculate_dist(&(prim->cyl.origin), &(prim->p))) -
+	dist = SQR(ft_calculate_dist(&(prim->cyl.origin), p)) -
 														SQR(prim->cyl.radius);
 	if (dist >= 0)
 		dist = sqrt(dist);
 	prim->cyl.o.x = prim->cyl.origin_local.x + dist * prim->cyl.vec_local.x;
 	prim->cyl.o.y = prim->cyl.origin_local.y + dist * prim->cyl.vec_local.y;
 	prim->cyl.o.z = prim->cyl.origin_local.z + dist * prim->cyl.vec_local.z;
-	ft_swap_g_to_l(&local_p, &(prim->p),
+	ft_swap_g_to_l(&local_p, p,
 									&(prim->g_to_l_move), &(prim->g_to_l_rot));
 	if (local_p.y < prim->cyl.origin_local.y)
 		prim->cyl.o.y = prim->cyl.origin_local.y +
@@ -36,16 +36,16 @@ void	ft_cylinder_normal(t_prim *prim)
 	ft_swap_l_to_g(&global_o, &(prim->cyl.o),
 									&(prim->l_to_g_move), &(prim->l_to_g_rot));
 	ft_set_3dpt(&(prim->cyl.o), global_o.x, global_o.y, global_o.z);
-	ft_calculate_vector(&(prim->normal), &(prim->cyl.o), &(prim->p));
+	ft_calculate_vector(&(prim->normal), &(prim->cyl.o), p);
 }
 
-void	ft_cone_normal(t_prim *prim)
+void	ft_cone_normal(t_prim *prim, t_3dpt *p)
 {
 	double	dist;
 	t_3dpt	local_p;
 	t_3dpt	global_o;
 
-	ft_swap_g_to_l(&local_p, &(prim->p), &(prim->g_to_l_move),
+	ft_swap_g_to_l(&local_p, p, &(prim->g_to_l_move),
 														&(prim->g_to_l_rot));
 	if (cos(prim->cone.angle) == 0)
 		ft_error(ERRDIV);
@@ -63,19 +63,18 @@ void	ft_cone_normal(t_prim *prim)
 	ft_swap_l_to_g(&global_o, &(prim->cone.o),
 									&(prim->l_to_g_move), &(prim->l_to_g_rot));
 	ft_set_3dpt(&(prim->cone.o), global_o.x, global_o.y, global_o.z);
-	ft_calculate_vector(&(prim->normal), &(prim->cone.o), &(prim->p));
+	ft_calculate_vector(&(prim->normal), &(prim->cone.o), p);
 }
 
-void	ft_calculate_normal(t_prim *prim)
+void	ft_calculate_normal(t_prim *prim, t_3dpt *p)
 {
 	if (prim->type == SPHERE)
-		ft_calculate_vector(&(prim->normal), &(prim->sphere.origin),
-																&(prim->p));
+		ft_calculate_vector(&(prim->normal), &(prim->sphere.origin), p);
 	else if (prim->type == PLANE)
 		ft_set_3dpt(&(prim->normal), prim->plane.normal.x,
 					prim->plane.normal.y, prim->plane.normal.z);
 	else if (prim->type == CYLINDER)
-		ft_cylinder_normal(prim);
+		ft_cylinder_normal(prim, p);
 	else if (prim->type == CONE)
-		ft_cone_normal(prim);
+		ft_cone_normal(prim, p);
 }
