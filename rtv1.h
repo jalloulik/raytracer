9a7aaf07d6d8a643c0d012c67dc498b67e1d8fdd
Kais Jallouli <kjalloul@42.student.fr>
2018-06-15 16:48:53 +0200

    fixed issue with inconsistent checkers

--- rtv1.h
+++ rtv1.h
@@ -6,7 +6,7 @@
 /*   By: kjalloul <kjalloul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/08 12:04:54 by kjalloul          #+#    #+#             */
-/*   Updated: 2018/06/13 09:56:43 by kjalloul         ###   ########.fr       */
+/*   Updated: 2018/06/15 16:28:35 by kjalloul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -497,6 +497,6 @@ void				ft_sine_perturbation(t_prim *prim, t_3dpt *p);
 
 t_color				ft_get_prim_texture_checker(t_prim *prim);
 void				ft_get_prim_texture_color_main(t_prim *prim);
-t_color				ft_get_checkers_color(double x, double y);
+t_color		ft_get_checkers_color(double x, double y, t_texture *textur);
 
 #endif
--- src/ft_global_local_swap.c
+++ src/ft_global_local_swap.c
@@ -6,7 +6,7 @@
 /*   By: kjalloul <kjalloul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/21 15:28:11 by kjalloul          #+#    #+#             */
-/*   Updated: 2018/06/13 13:40:04 by kjalloul         ###   ########.fr       */
+/*   Updated: 2018/06/13 13:45:33 by kjalloul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,19 +51,13 @@ void	ft_create_local_vector_spaces(t_prim *prim)
 	while (prim != NULL)
 	{
 		if (prim->type == SPHERE)
-		{
 			ft_create_local_sphere(prim);
-		}
 		else if (prim->type == PLANE)
-		{
 			ft_create_local_plane(prim);
-		}
 		else if (prim->type == CYLINDER)
 			ft_create_local_cyl(prim);
 		else if (prim->type == CONE)
-		{
 			ft_create_local_cone(prim);
-		}
 		else if (prim->type == RECT)
 			ft_create_local_rect(prim);
 		else if (prim->type == TORE)
