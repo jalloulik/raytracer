dc3519c1f1115db7f90c7de4c8fb2cc9786fc200
Teddy FAVART <tfavart@e1r13p4.42.fr>
2018-06-24 17:22:52 +0200

    mise a la norme ui et lib xml

--- libxml/xmlp.c
+++ libxml/xmlp.c
@@ -6,7 +6,7 @@
 /*   By: tfavart <tfavart@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 18:14:18 by mfrisby           #+#    #+#             */
-/*   Updated: 2018/06/24 16:55:35 by tfavart          ###   ########.fr       */
+/*   Updated: 2018/06/24 17:12:04 by tfavart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_xmlp		*new_xmlp(char *path)
 	fd = open_file(path);
 	if (fd < 0)
 		return (NULL);
-	if(!(xmlp = malloc(sizeof(t_xmlp))))
+	if (!(xmlp = malloc(sizeof(t_xmlp))))
 		ft_error_2("probleme allocation memoire\n");
 	xmlp->fd = fd;
 	xmlp->path = path;
